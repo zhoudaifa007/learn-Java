acea2eaa7290ae0e6d13c3ad61621a2e9ee88a9b
zhoudaifa007 <zhoudaifa007@gmail.com>
2017-07-09 11:01:54 +0800

    数组交集

---
+++ com.zhou.daqin.jueqi/com.zhou.daqin.jueqi.alibaba/src/main/java/com/zhou/daqin/jueqi/alibaba/leetcode/Intersection.java
@@ -0,0 +1,74 @@
+package com.zhou.daqin.jueqi.alibaba.leetcode;
+
+/**
+ * Created by zhoudf2 on 2017-7-9.
+ */
+
+import java.util.*;
+
+/**
+ *
+ * 349. Intersection of Two Arrays
+ */
+public class Intersection {
+    public int[] intersection(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+        int[] arr = new int[0];
+        int index1 = 0;
+        int index2 = 0;
+        Set<Integer> set = new HashSet<>();
+
+        while(index1 < nums1.length && index2 < nums2.length)
+        {
+            int num1 = nums1[index1];
+            int num2 = nums2[index2];
+            if(num1 > num2)
+            {
+                index2 = index2 + 1;
+            }
+            else if(num1 < num2)
+            {
+                index1 = index1 + 1;
+            } else
+            {
+                set.add(nums1[index1]);
+                index1 = index1 + 1;
+                index2 = index2 + 1;
+            }
+        }
+
+        index1 = 0;
+        index2 = 0;
+        while(index1 < nums1.length && index2 < nums2.length)
+        {
+            int num1 = nums1[index1];
+            int num2 = nums2[index2];
+            if(num1 < num2)
+            {
+                index2 = index2 + 1;
+            }
+            else if(num1 > num2)
+            {
+                index1 = index1 + 1;
+            } else
+            {
+                set.add(nums1[index1]);
+                index1 = index1 + 1;
+                index2 = index2 + 1;
+            }
+        }
+
+        if(set.size() > 0)
+        {
+            arr = new int[set.size()];
+            int i = 0;
+            for(Integer ele: set)
+            {
+                arr[i] = ele;
+                i++;
+            }
+        }
+        return  arr;
+    }
+}
---
+++ com.zhou.daqin.jueqi/com.zhou.daqin.jueqi.alibaba/src/test/java/com/zhou/daqin/jueqi/alibaba/leetcode/IntersectionTest.java
@@ -0,0 +1,53 @@
+package com.zhou.daqin.jueqi.alibaba.leetcode;
+
+import com.alibaba.fastjson.JSONObject;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Created by zhoudf2 on 2017-7-9.
+ */
+@RunWith(SpringRunner.class)
+public class IntersectionTest {
+
+    @Test
+    public void test1()
+    {
+        int[] arr1 = {1, 2, 2, 1};
+        int[] arr2 = {2, 2};
+        Intersection in = new Intersection();
+        int[] array =  in.intersection(arr1,arr2);
+        System.out.println(JSONObject.toJSONString(array));
+    }
+
+    @Test
+    public void test2()
+    {
+        int[] arr1 = {};
+        int[] arr2 = {};
+        Intersection in = new Intersection();
+        int[] array =  in.intersection(arr1,arr2);
+        System.out.println(JSONObject.toJSONString(array));
+    }
+
+    @Test
+    public void test3()
+    {
+        int[] arr1 = {2, 1};
+        int[] arr2 = {1, 1};
+        Intersection in = new Intersection();
+        int[] array =  in.intersection(arr1,arr2);
+        System.out.println(JSONObject.toJSONString(array));
+    }
+
+    @Test
+    public void test4()
+    {
+        int[] arr1 = {4,7,9,7,6,7};
+        int[] arr2 = {5,0,0,6,1,6,2,2,4};
+        Intersection in = new Intersection();
+        int[] array =  in.intersection(arr1,arr2);
+        System.out.println(JSONObject.toJSONString(array));
+    }
+}
