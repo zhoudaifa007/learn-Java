1b7b7ec35199df4c312047da284ad942b307c681
zhoudaifa007 <zhoudaifa007@gmail.com>
2018-03-23 09:25:08 +0800

    添加文件

---
+++ com.zhou.daqin.jueqi/com.zhou.daqin.jueqi.alibaba/src/main/java/com/zhou/daqin/jueqi/alibaba/collection/TreeMapPractice.java
@@ -0,0 +1,7 @@
+package com.zhou.daqin.jueqi.alibaba.collection;
+
+/**
+ * Created by zhoudf2 on 2017-7-9.
+ */
+public class TreeMapPractice {
+}
--- com.zhou.daqin.jueqi/com.zhou.daqin.jueqi.alibaba/src/test/java/com/zhou/daqin/jueqi/alibaba/collection/CollectionTest.java
+++ com.zhou.daqin.jueqi/com.zhou.daqin.jueqi.alibaba/src/test/java/com/zhou/daqin/jueqi/alibaba/collection/CollectionTest.java
@@ -1,7 +1,36 @@
 package com.zhou.daqin.jueqi.alibaba.collection;
 
+import org.junit.Test;
+
+import java.util.concurrent.ArrayBlockingQueue;
+
+
 /**
  * Created by zhoudf2 on 2017-7-8.
  */
+
 public class CollectionTest {
+
+    @Test
+    public void test1(){
+        System.out.println("fdsafdsafdsaf");
+    }
+
+    @Test
+    public void test2(){
+        ArrayBlockingQueue<Integer> arrayBlockingQueue = new ArrayBlockingQueue<Integer>(5);
+        arrayBlockingQueue.add(1);
+        arrayBlockingQueue.add(2);
+        arrayBlockingQueue.add(3);
+        arrayBlockingQueue.add(4);
+        arrayBlockingQueue.add(5);
+        arrayBlockingQueue.poll();
+        System.out.println("add 5");
+        try {
+            arrayBlockingQueue.put(6);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println("add 6");
+    }
 }
